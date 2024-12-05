d9acd0486b0bd1b356d8d16fda2567b55276bda4
antoniaand <andreesculoghinantonia23@stud.ase.ro>
2024-12-05 14:12:17 +0000

    command identify

--- TechVault--OOP-Project/TechVault--OOP-Project/TechVault--OOP-Project.cpp
+++ TechVault--OOP-Project/TechVault--OOP-Project/TechVault--OOP-Project.cpp
@@ -2,13 +2,60 @@
 //
 
 #include <iostream>
+#include<string>
+
+
 using namespace std;
+
+//which will also be the CLASSES NAMES
+enum CommandType { CREATE_TABLE, DROP_TABLE, DISPLAY_TABLE, CREATE_INDEX, DROP_INDEX, INSERT, SELECT, UPDATE, DELETE, WRONG };
+
+CommandType identify(string command) {
+    string cmd = command; //a copy
+    for (int i = 0; i < strlen(command); i++)
+        cmd[i] = toupper(cmd[i]);
+
+    if (strncmp(cmd, "CREATE TABLE", 12) == 0) {
+        return CREATE_TABLE;       //it returns one of the enum types
+    }
+    else if (strncmp(cmd, "DROP TABLE", 10) == 0) {
+        return DROP_TABLE;
+    }
+    else if (strncmp(cmd, "DISPLAY TABLE", 13) == 0) {
+        return DISPLAY_TABLE;
+    }
+    else if (strncmp(cmd, "CREATE INDEX", 12) == 0) {
+        return CREATE_INDEX;
+    }
+    else if (strncmp(cmd, "DROP INDEX", 10) == 0) {
+        return DROP_INDEX;
+    }
+    else if (strncmp(cmd, "INSERT", 6) == 0) {
+        return INSERT;
+    }
+    else if (strncmp(cmd, "SELECT", 6) == 0) {
+        return SELECT;
+    }
+    else if (strncmp(cmd, "UPDATE", 6) == 0) {
+        return UPDATE;
+    }
+    else if (strncmp(cmd, "DELETE", 6) == 0) {
+        return DELETE;
+    }
+    else {
+        return WRONG;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!!!  incepem proiectu\n";
-    std::cout << endl << "Testare";
-    std::cout << endl << "Nia, tot nu merge.  --Maria";
-    std::cout << "merge! - nia";
+    string command;
+    cout << "Enter a SQL command: " << endl;
+    getline(cin, command);
+    CommandType type = identify(command);
+    cout << endl<<"Command Type is:  " << type;
+
+
 }
+
 
